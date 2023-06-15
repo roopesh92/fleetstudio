eac3d3930e740e767db7ae1e130568e96f2862c1
roopesh92 <77221351+roopesh92@users.noreply.github.com>
2023-06-15 19:56:05 +0530

    Update CountyRepository.java

--- src/main/java/com/fleetstudio/county/Repositories/CountyRepository.java
+++ src/main/java/com/fleetstudio/county/Repositories/CountyRepository.java
@@ -12,6 +12,9 @@ public interface CountyRepository extends CrudRepository<County, Long> {
     @Query(value = "SELECT c FROM County c WHERE lower(c.state) LIKE ?1 OR lower(c.name) LIKE ?1")
     List<County> getCountySuggestion(String query,Pageable pageable);
 
+    @Query(value = "SELECT c FROM County c WHERE lower(c.state) LIKE ?1 AND lower(c.name) LIKE ?2")
+    List<County> getCountySuggestionByStateAndName(String state,String name,Pageable pageable);
+
     List<County> findByStateContainingIgnoreCase(String title,Pageable pageable);
 
     List<County> findByNameContainingIgnoreCase(String title,Pageable pageable);
